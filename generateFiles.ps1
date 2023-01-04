848bd7acb8937c806c7aae5d9f4c5c004bb42bcc
santiago <santivasquez1@gmail.com>
2023-01-04 16:24:45 -0500

    Automatizacion creacion docker-compose

--- generateFiles.ps1
+++ generateFiles.ps1
@@ -3,6 +3,7 @@ $networkNodes = "networkNodes";
 $EnodeParams = "Enodes.params"
 $UrlsParams = "Urls.params";
 $contractsBuild = "build";
+$networkFiles = "./besu-config/networkFiles";
 $certs = "certs";
 $numNodes = $args[0];
 $nodePrefix = $args[1];
@@ -30,6 +31,10 @@ if (Test-Path -Path $EnodeParams) {
 
 if (Test-Path -Path $UrlsParams) {
     Remove-Item  $UrlsParams -Recurse -Force;
+}
+
+if (Test-Path -Path $networkFiles) {
+    Remove-Item  $networkFiles -Recurse -Force
 }
 
 docker compose -f "./initial-besu.yaml" down;
@@ -63,6 +68,35 @@ if ($null -eq $gateWay) {
     $gateWay = "172.21.0.1"
 }
 
+$nodeProperties = [ordered]@{
+    image          = "hyperledger/besu:22.10.0-RC2"
+    container_name = ""
+    user           = "root" 
+    volumes        = @()
+    ports          = @()
+    networks       = $null
+    entrypoint     = @("/bin/bash", "-c");
+}
+
+$network = [pscustomobject] @{
+    rpc = [pscustomobject] @{
+        driver = "bridge"
+        ipam   = [pscustomobject] @{
+            driver = "default"
+            config = @([pscustomobject] @{
+                    subnet  = "172.21.0.0/16"
+                    gateway = $gateWay
+                })
+        }
+    }
+};   
+
+$networkTemplate = [pscustomobject] @{
+    version  = "3"
+    services = New-Object -TypeName PSCustomObject
+    networks = $network
+};
+
 for ($i = 0; $i -lt $numNodes; $i++) {
     $nodeName = $nodePrefix + ($i + 1);
 
@@ -78,24 +112,37 @@ for ($i = 0; $i -lt $numNodes; $i++) {
     
     $p2pNumber = $p2pDefault + $i;
     $rcpNumber = $rpcDefault + $i;
-    $p2pPort = "p2p-port=" + $p2pNumber;
-    $rpchttpport = "rpc-http-port=" + $rcpNumber;
-    $configContent = Get-Content -Path $workDir/$networkNodes/$nodeName/config.toml
-    $configContent -replace "p2p-port=P2PDEFAULT", $p2pPort | Set-Content -Path $workDir/$networkNodes/$nodeName/config.toml ;
+    $p2pPort = [string]$p2pNumber + ":" + [string]$p2pDefault;
+    $rpchttpport = [string]$rcpNumber + ":" + [string]$rpcDefault;
+    $nodeIp = $gateWay -replace "0.1", ("0." + ($i + 2));
 
-    $configContent = Get-Content -Path $workDir/$networkNodes/$nodeName/config.toml
-    $configContent -replace "rpc-http-port=RCPDEFAULT", $rpchttpport | Set-Content -Path $workDir/$networkNodes/$nodeName/config.toml ;
+    $nodeInstance = New-Object -TypeName PSCustomObject -Property $nodeProperties;
+    $nodeInstance.container_name = $nodeName + "_lab";
+    $nodeInstance.volumes += "./" + $networkNodes + "/" + $nodeName + "/:/opt/besu/" + $nodeName; 
+    $nodeInstance.ports += $p2pPort;
+    $nodeInstance.ports += $rpchttpport;
+    $nodeInstance.networks = [pscustomobject] @{
+        rpc = [pscustomobject] @{
+            ipv4_address = $nodeIp
+        }
+    }
 
+    $entrypoint = ("cd /opt/besu/" + $nodeName + "`nbesu --config-file=./config.toml`r") | ConvertTo-Yaml | ConvertFrom-Yaml;
+    $nodeInstance.entrypoint += $entrypoint
+    if ($i -gt 0) {
+        $nodeInstance | Add-Member -MemberType NoteProperty -Name "depends_on" -Value @($nodesOfNetwork[0])
+    }
+
+    $networkTemplate.services | Add-Member -MemberType NoteProperty -Name $nodeName -Value $nodeInstance
     $message = "Nodo " + $nodeName + " creado con exito"
     Write-Output  $message;
     $nodesOfNetwork += $nodeName;
 }
 
-$networkFiles = "./besu-config/networkFiles";
-
-if (Test-Path -Path $networkFiles) {
-    Remove-Item  $networkFiles -Recurse -Force
-}
+# $initialGenesis = Get-Content -Path $workDir/"initialGenesis.json" | ConvertFrom-Json;
+# $initialGenesis.blockchain.nodes.count = $numNodes;
+# $initialGenesis | ConvertTo-Json -Depth 3 | Out-File $workDir/"initialGenesis.json"
+$networkTemplate | ConvertTo-Yaml | Out-File $workDir/"set-network-docker-compose.yaml"
 
 docker compose -f "./initial-besu.yaml" up -d
 
@@ -107,6 +154,7 @@ while (-Not ($existNetworkFiles)) {
 }
 
 docker compose -f "./initial-besu.yaml" down;
+
 $keysDirs = Get-ChildItem $networkFiles/keys;
 $Counter = 0;
 New-Item $EnodeParams
@@ -120,8 +168,8 @@ for ($Counter = 0; $Counter -lt $keysDirs.Length; $Counter++) {
     Copy-Item $networkFiles/keys/$keyDir/key.pub ./$networkNodes/$nodePath/data
     Copy-Item $networkFiles/genesis.json ./$networkNodes/$nodePath
     
-    $p2pNumber = $p2pDefault + $Counter;
-    $rcpNumber = $rpcDefault + $Counter;
+    $p2pNumber = $p2pDefault;
+    $rcpNumber = $rpcDefault;
     $uri = "http://node" + ($Counter + 1) + ":" + $rcpNumber;
     $nodeIp = $gateWay -replace "0.1", ("0." + ($Counter + 2));
     $enodes += "enode://" + (Get-Content ./$networkNodes/$nodePath/data/key.pub).Substring(2) + "@" + $nodeIp + ":" + $p2pNumber;
@@ -144,6 +192,7 @@ $existsImage = docker images -q networkpostprocess:v1;
 if ( $null -eq $existsImage) {
     docker compose -f "./networkPostProcess/post-proces-docker-compose.yaml" build 
 }
+
 docker compose -f "./networkPostProcess/post-proces-docker-compose.yaml" up -d;
 $postProcessStatus = docker ps -f "status=exited" --format "{{.Names}}";
 while ($null -eq $postProcessStatus) {
@@ -155,23 +204,23 @@ while ($null -eq $postProcessStatus) {
 Start-Sleep 5;
 docker compose -f "./networkPostProcess/post-proces-docker-compose.yaml" down;
 
-$existsImage = docker images -q truffle:v1;
-if ( $null -eq $existsImage) {
-    docker compose -f "./DemoMvmTruffle_Fork/truffle-docker-compose.yaml" build;
-}
+# $existsImage = docker images -q truffle:v1;
+# if ( $null -eq $existsImage) {
+#     docker compose -f "./DemoMvmTruffle_Fork/truffle-docker-compose.yaml" build;
+# }
     
-docker compose -f "./DemoMvmTruffle_Fork/truffle-docker-compose.yaml" up -d 
+# docker compose -f "./DemoMvmTruffle_Fork/truffle-docker-compose.yaml" up -d 
 
-$prueba = docker logs truffle-container --tail 1;
-while (-not($prueba -eq "Im up")) {
-    Write-Output "Esperando a que se generen los archivos de configuracion";
-    Start-Sleep 5;
-    $prueba = docker logs truffle-container --tail 1;
-}
+# $prueba = docker logs truffle-container --tail 1;
+# while (-not($prueba -eq "Im up")) {
+#     Write-Output "Esperando a que se generen los archivos de configuracion";
+#     Start-Sleep 5;
+#     $prueba = docker logs truffle-container --tail 1;
+# }
 
-Copy-Item -Recurse -Force -Path $workDir/$contractsBuild $workDir/"DemoEnergiaMVMFrontFork";
+# Copy-Item -Recurse -Force -Path $workDir/$contractsBuild $workDir/"DemoEnergiaMVMFrontFork";
     
-docker compose -f "./DemoEnergiaMVMFrontFork/frontend-docker-compose.yaml" build;
-docker compose -f "./DemoEnergiaMVMFrontFork/frontend-docker-compose.yaml" up -d;
+# docker compose -f "./DemoEnergiaMVMFrontFork/frontend-docker-compose.yaml" build;
+# docker compose -f "./DemoEnergiaMVMFrontFork/frontend-docker-compose.yaml" up -d;
 Write-Output "Fin del proceso";
 
